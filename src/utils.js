4da14673a5de6f97b18f6770890c5df6c2626536
hamednn76 <hamed.navabian@gmail.com>
2020-10-25 03:10:09 +0330

    remove comments

--- src/utils.js
+++ src/utils.js
@@ -7,13 +7,3 @@ export function actionTypesGenerator(name) {
     resetForm: `${name}/RESET_FORM`,
   };
 }
-
-/*export function createReducer(initialState, handlers) {
-  return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action);
-    } else {
-      return state;
-    }
-  };
-}*/
